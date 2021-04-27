92b14e2a266c1128b5854f1056bf602d26f2fb27
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2021-04-27 14:28:35 +0300

    Added two more handlers

--- handlers.go
+++ handlers.go
@@ -4,11 +4,29 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 func DefaultHandler(rw http.ResponseWriter, r *http.Request) {
 	log.Println("Serving:", r.URL.Path, "from", r.Host, "with method", r.Method)
 	rw.WriteHeader(http.StatusNotFound)
 	Body := r.URL.Path + " is not supported. Thanks for visiting!\n"
+	fmt.Fprintf(rw, "%s", Body)
+}
+
+// MethodNotAllowedHandler is executed when the method is incorrect
+func MethodNotAllowedHandler(rw http.ResponseWriter, r *http.Request) {
+	log.Println("Serving:", r.URL.Path, "from", r.Host, "with method", r.Method)
+	rw.WriteHeader(http.StatusNotFound)
+	Body := "Method not allowed!\n"
+	fmt.Fprintf(rw, "%s", Body)
+}
+
+// TimeHandler is for handling /time
+func TimeHandler(rw http.ResponseWriter, r *http.Request) {
+	log.Println("Serving:", r.URL.Path, "from", r.Host)
+	rw.WriteHeader(http.StatusOK)
+	t := time.Now().Format(time.RFC1123)
+	Body := "The current time is: " + t + "\n"
 	fmt.Fprintf(rw, "%s", Body)
 }

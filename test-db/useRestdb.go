1bb2e914947256ff81e993c14f7aa6be90d6fdbf
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2021-05-11 00:28:42 +0300

    Minor correction

--- test-db/useRestdb.go
+++ test-db/useRestdb.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 
 	_ "github.com/lib/pq"
 	"github.com/mactsouk/restdb"
@@ -16,8 +18,32 @@ type User struct {
 	Active    int
 }
 
+// PostgreSQL Connection details
+var (
+	Hostname = "localhost"
+	Port     = 5432
+	Username = "mtsouk"
+	Password = "pass"
+	Database = "restapi"
+)
+
+func ConnectPostgres() *sql.DB {
+	conn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", Hostname, Port, Username, Password, Database)
+
+	db, err := sql.Open("postgres", conn)
+	if err != nil {
+		log.Println(err)
+		return nil
+	}
+
+	return db
+}
+
 func main() {
 	db := restdb.ConnectPostgres()
+	fmt.Println(db)
+
+	db = ConnectPostgres()
 	fmt.Println(db)
 
 	t := restdb.User{}

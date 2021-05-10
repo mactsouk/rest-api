e8fa3f4a70daf0bdba93f76dceb22225b6e7fdba
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2021-05-11 00:36:50 +0300

    Better testing

--- test-db/useRestdb.go
+++ test-db/useRestdb.go
@@ -42,10 +42,37 @@ func ConnectPostgres() *sql.DB {
 func main() {
 	db := restdb.ConnectPostgres()
 	fmt.Println(db)
+	db.Close()
 
 	db = ConnectPostgres()
 	fmt.Println(db)
+	defer db.Close()
 
 	t := restdb.User{}
 	fmt.Println(t)
+
+	rows, err := db.Query(`SELECT "username" FROM "users"`)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer rows.Close()
+	for rows.Next() {
+		var username string
+
+		err = rows.Scan(&username)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Println(name)
+	}
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
 }

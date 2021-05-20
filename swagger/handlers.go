aec4b39f776c7d121ed0d817b046d90dbc2a1cdc
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2021-05-20 08:47:38 +0300

    Added Swagger metadata

--- swagger/handlers.go
+++ swagger/handlers.go
@@ -1,3 +1,18 @@
+// Package main For the RESTful Server
+//
+// Documentation for REST API
+//
+//	Schemes: http
+//	BasePath: /
+//	Version: 1.0.5
+//
+//	Consumes:
+//	- application/json
+//
+//	Produces:
+//	- application/json
+//
+// swagger:meta
 package main
 
 import (
@@ -13,13 +28,39 @@ import (
 	"github.com/mactsouk/restdb"
 )
 
+// @termsOfService http://swagger.io/terms/
+
+// User defines the structure for a Full User Record
+// swagger:model User
 type User struct {
-	ID        int    `json:"id"`
-	Username  string `json:"user"`
-	Password  string `json:"password"`
-	LastLogin int64  `json:"lastlogin"`
-	Admin     int    `json:"admin"`
-	Active    int    `json:"active"`
+	// The ID for the User
+	// in: body
+	//
+	// required: false
+	// min: 1
+	ID int `json:"id"`
+	// The Username of the User
+	// in: body
+	//
+	// required: true
+	Username string `json:"user"`
+	// The Password of the User
+	//
+	// required: true
+	Password string `json:"password"`
+	// The Last Login time of the User
+	//
+	// required: true
+	// min: 0
+	LastLogin int64 `json:"lastlogin"`
+	// Is the User Admin or not
+	//
+	// required: true
+	Admin int `json:"admin"`
+	// Is the User Logged In or Not
+	//
+	// required: true
+	Active int `json:"active"`
 }
 
 // SliceToJSON encodes a slice with JSON records
@@ -34,12 +75,25 @@ func (h notAllowedHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	MethodNotAllowedHandler(rw, r)
 }
 
+// swagger:route DELETE / Anything EMPTY
+// Default Handler for everything that is not a match
+//
+// responses:
+// 404: ErrorMessage
+
+// DefaultHandler is for handling everything that is not a match
 func DefaultHandler(rw http.ResponseWriter, r *http.Request) {
 	log.Println("DefaultHandler Serving:", r.URL.Path, "from", r.Host, "with method", r.Method)
 	rw.WriteHeader(http.StatusNotFound)
 	Body := r.URL.Path + " is not supported. Thanks for visiting!\n"
 	fmt.Fprintf(rw, "%s", Body)
 }
+
+// swagger:route GET /* NULL
+// Default Handler for endpoints used with incorrect HTTP request method
+//
+// responses:
+//	404: ErrorMessage
 
 // MethodNotAllowedHandler is executed when the HTTP method is incorrect
 func MethodNotAllowedHandler(rw http.ResponseWriter, r *http.Request) {
@@ -49,6 +103,12 @@ func MethodNotAllowedHandler(rw http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(rw, "%s", Body)
 }
 
+// swagger:route GET /time time NULL
+// Return current time
+//
+// responses:
+//	200: OK
+
 // TimeHandler is for handling /time – it works with plain text
 func TimeHandler(rw http.ResponseWriter, r *http.Request) {
 	log.Println("TimeHandler Serving:", r.URL.Path, "from", r.Host)
@@ -57,6 +117,13 @@ func TimeHandler(rw http.ResponseWriter, r *http.Request) {
 	Body := "The current time is: " + t + "\n"
 	fmt.Fprintf(rw, "%s", Body)
 }
+
+// swagger:route POST /add createUser User
+// Create a new user
+//
+// responses:
+//	200: OK
+//  400: BadRequest
 
 // AddHandler is for adding a new user
 func AddHandler(rw http.ResponseWriter, r *http.Request) {
@@ -184,6 +251,13 @@ func GetAllHandler(rw http.ResponseWriter, r *http.Request) {
 		return
 	}
 }
+
+// swagger:route GET /getid Username-Password User
+// Returns the ID of a user given their username
+//
+// responses:
+//	200: OK
+//  400: BadRequest
 
 // GetIDHandler returns the ID of an existing user
 func GetIDHandler(rw http.ResponseWriter, r *http.Request) {
@@ -314,6 +388,13 @@ func UpdateHandler(rw http.ResponseWriter, r *http.Request) {
 	rw.WriteHeader(http.StatusOK)
 }
 
+// swagger:route POST /login Username-Password User
+// Login an existing user
+//
+// responses:
+//	200: OK
+//  400: BadRequest
+
 // LoginHandler is for updating the LastLogin time of a user
 // And changing the Active field to true
 func LoginHandler(rw http.ResponseWriter, r *http.Request) {
@@ -429,4 +510,28 @@ func LoggedUsersHandler(rw http.ResponseWriter, r *http.Request) {
 		rw.WriteHeader(http.StatusBadRequest)
 		return
 	}
+}
+
+// Generic OK message returned as an HTTP Status Code
+// swagger:response OK
+type OK struct {
+	// Description of the situation
+	// in: body
+	Body int
+}
+
+// Generic error message returned as an HTTP Status Code
+// swagger:response ErrorMessage
+type ErrorMessage struct {
+	// Description of the situation
+	// in: body
+	Body int
+}
+
+// Generic BadRequest message returned as an HTTP Status Code
+// swagger:response BadRequest
+type BadRequest struct {
+	// Description of the situation
+	// in: body
+	Body int
 }

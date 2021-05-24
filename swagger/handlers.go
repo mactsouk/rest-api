9f0f78f70a32434f1fca8e24c9ef1733880bff2d
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2021-05-24 08:23:32 +0300

    Cleanup swagger file

--- swagger/handlers.go
+++ swagger/handlers.go
@@ -21,7 +21,6 @@ import (
 	"io"
 	"log"
 	"net/http"
-	"strconv"
 	"time"
 
 	"github.com/gorilla/mux"
@@ -166,92 +165,6 @@ func AddHandler(rw http.ResponseWriter, r *http.Request) {
 	}
 }
 
-// DeleteHandler is for deleting an existing user + DELETE
-func DeleteHandler(rw http.ResponseWriter, r *http.Request) {
-	log.Println("DeleteHandler Serving:", r.URL.Path, "from", r.Host)
-
-	// Get the ID of the user to be deleted
-	id, ok := mux.Vars(r)["id"]
-	if !ok {
-		log.Println("ID value not set!")
-		rw.WriteHeader(http.StatusNotFound)
-		return
-	}
-
-	var user = restdb.User{}
-	err := user.FromJSON(r.Body)
-	if err != nil {
-		log.Println(err)
-		rw.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	if !restdb.IsUserAdmin(user) {
-		log.Println("User", user.Username, "is not admin!")
-		rw.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	intID, err := strconv.Atoi(id)
-	if err != nil {
-		log.Println("id", err)
-		return
-	}
-
-	t := restdb.FindUserID(intID)
-	if t.Username != "" {
-		log.Println("About to delete:", t)
-		deleted := restdb.DeleteUser(intID)
-		if deleted {
-			log.Println("User deleted:", id)
-			rw.WriteHeader(http.StatusOK)
-			return
-		} else {
-			log.Println("User ID not found:", id)
-			rw.WriteHeader(http.StatusNotFound)
-		}
-	}
-	rw.WriteHeader(http.StatusNotFound)
-}
-
-// GetAllHandler is for getting all data from the user database
-func GetAllHandler(rw http.ResponseWriter, r *http.Request) {
-	log.Println("GetAllHandler Serving:", r.URL.Path, "from", r.Host)
-	d, err := io.ReadAll(r.Body)
-	if err != nil {
-		rw.WriteHeader(http.StatusBadRequest)
-		log.Println(err)
-		return
-	}
-
-	if len(d) == 0 {
-		rw.WriteHeader(http.StatusBadRequest)
-		log.Println("No input!")
-		return
-	}
-
-	var user = restdb.User{}
-	err = json.Unmarshal(d, &user)
-	if err != nil {
-		log.Println(err)
-		rw.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	if !restdb.IsUserAdmin(user) {
-		log.Println("User", user, "is not an admin!")
-		rw.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	err = SliceToJSON(restdb.ListAllUsers(), rw)
-	if err != nil {
-		log.Println(err)
-		rw.WriteHeader(http.StatusBadRequest)
-		return
-	}
-}
-
 // swagger:route GET /getid Username-Password User
 // Returns the ID of a user given their username
 //
@@ -311,83 +224,6 @@ func GetIDHandler(rw http.ResponseWriter, r *http.Request) {
 	}
 }
 
-// GetUserDataHandler + GET returns the full record of a user
-func GetUserDataHandler(rw http.ResponseWriter, r *http.Request) {
-	log.Println("GetUserDataHandler Serving:", r.URL.Path, "from", r.Host)
-	id, ok := mux.Vars(r)["id"]
-	if !ok {
-		log.Println("ID value not set!")
-		rw.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	intID, err := strconv.Atoi(id)
-	if err != nil {
-		log.Println("id", err)
-		rw.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	t := restdb.FindUserID(intID)
-	if t.ID != 0 {
-		err := t.ToJSON(rw)
-		if err != nil {
-			rw.WriteHeader(http.StatusBadRequest)
-			log.Println(err)
-		}
-		return
-	}
-
-	log.Println("User not found:", id)
-	rw.WriteHeader(http.StatusBadRequest)
-}
-
-// UpdateHandler is for updating the data of an existing user + PUT
-func UpdateHandler(rw http.ResponseWriter, r *http.Request) {
-	log.Println("UpdateHandler Serving:", r.URL.Path, "from", r.Host)
-	d, err := io.ReadAll(r.Body)
-	if err != nil {
-		rw.WriteHeader(http.StatusBadRequest)
-		log.Println(err)
-		return
-	}
-
-	if len(d) == 0 {
-		rw.WriteHeader(http.StatusBadRequest)
-		log.Println("No input!")
-		return
-	}
-
-	var users = []restdb.User{}
-	err = json.Unmarshal(d, &users)
-	if err != nil {
-		log.Println(err)
-		rw.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	if !restdb.IsUserAdmin(users[0]) {
-		log.Println("Command issued by non-admin user:", users[0].Username)
-		rw.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	log.Println(users)
-	t := restdb.FindUserUsername(users[1].Username)
-	t.Username = users[1].Username
-	t.Password = users[1].Password
-	t.Admin = users[1].Admin
-
-	if !restdb.UpdateUser(t) {
-		log.Println("Update failed:", t)
-		rw.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	log.Println("Update successful:", t)
-	rw.WriteHeader(http.StatusOK)
-}
-
 // swagger:route POST /login Username-Password User
 // Login an existing user
 //
@@ -439,76 +275,6 @@ func LoginHandler(rw http.ResponseWriter, r *http.Request) {
 	} else {
 		log.Println("Update failed:", t)
 		rw.WriteHeader(http.StatusBadRequest)
-	}
-}
-
-// LogoutHandler is for logging out a user
-// And changing the Active field to false
-func LogoutHandler(rw http.ResponseWriter, r *http.Request) {
-	log.Println("LogoutHandler Serving:", r.URL.Path, "from", r.Host)
-
-	d, err := io.ReadAll(r.Body)
-	if err != nil {
-		rw.WriteHeader(http.StatusBadRequest)
-		log.Println(err)
-		return
-	}
-
-	if len(d) == 0 {
-		rw.WriteHeader(http.StatusBadRequest)
-		log.Println("No input!")
-		return
-	}
-
-	var user = restdb.User{}
-	err = json.Unmarshal(d, &user)
-	if err != nil {
-		log.Println(err)
-		rw.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	if !restdb.IsUserValid(user) {
-		log.Println("User", user.Username, "exists!")
-		rw.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	t := restdb.FindUserUsername(user.Username)
-	log.Println("Logging out:", t.Username)
-	t.Active = 0
-	if restdb.UpdateUser(t) {
-		log.Println("User updated:", t)
-		rw.WriteHeader(http.StatusOK)
-	} else {
-		log.Println("Update failed:", t)
-		rw.WriteHeader(http.StatusBadRequest)
-	}
-}
-
-// LoggedUsersHandler returns the list of all logged in users
-func LoggedUsersHandler(rw http.ResponseWriter, r *http.Request) {
-	log.Println("LoggedUsersHandler Serving:", r.URL.Path, "from", r.Host)
-	var user = restdb.User{}
-	err := user.FromJSON(r.Body)
-
-	if err != nil {
-		log.Println(err)
-		rw.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	if !restdb.IsUserValid(user) {
-		log.Println("User", user.Username, "exists!")
-		rw.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	err = SliceToJSON(restdb.ReturnLoggedUsers(), rw)
-	if err != nil {
-		log.Println(err)
-		rw.WriteHeader(http.StatusBadRequest)
-		return
 	}
 }
 
